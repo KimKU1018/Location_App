18970e2069de30f119375376cb72ad702232631c
KimKU1018 <86593582+KimKU1018@users.noreply.github.com>
2023-10-25 23:58:02 +0900

    Initial Commit

---
+++ Location_App/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Location_App
+//
+//  Created by KU Kim on 2023/10/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
